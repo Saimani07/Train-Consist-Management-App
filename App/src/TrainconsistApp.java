e327dd1eafb0fafb43be0a8cde2da85e9ff8de7e
Saimani07 <gurramsai2007@gmail.com>
2026-03-26 14:36:40 +0530

    UC-4 READY

--- App/src/TrainconsistApp.java
+++ App/src/TrainconsistApp.java
@@ -1,18 +1,22 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.LinkedList;
 
-public class TrainconsistApp {
+public class TrainconsistApp{
     public static void main(String[] args) {
 
-        Set<String> bogieIds = new HashSet<>();
+        LinkedList<String> train = new LinkedList<>();
 
-        bogieIds.add("BG101");
-        bogieIds.add("BG102");
-        bogieIds.add("BG103");
-        bogieIds.add("BG101");
-        bogieIds.add("BG102");
+        train.add("Engine");
+        train.add("Sleeper");
+        train.add("AC");
+        train.add("Cargo");
+        train.add("Guard");
 
-        System.out.println("Unique Bogie IDs:");
-        System.out.println(bogieIds);
+        train.add(2, "Pantry");
+
+        train.removeFirst();
+        train.removeLast();
+
+        System.out.println("Final Train Consist:");
+        System.out.println(train);
     }
 }
