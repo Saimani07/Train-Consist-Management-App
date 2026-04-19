54f9c7e533ecbaac324f1b5534c50b96da8827a0
Saimani07 <gurramsai2007@gmail.com>
2026-04-19 16:59:06 +0530

    UC-16 IS READY

--- App/src/TrainconsistAppTest.java
+++ App/src/TrainconsistAppTest.java
@@ -1,50 +1,55 @@
 public class TrainconsistAppTest {
 
     public static void main(String[] args) {
-        testCargo_SafeAssignment();
-        testCargo_UnsafeAssignmentHandled();
-        testCargo_CargoNotAssignedAfterFailure();
-        testCargo_ProgramContinuesAfterException();
-        testCargo_FinallyBlockExecution();
+        testSort_BasicSorting();
+        testSort_AlreadySortedArray();
+        testSort_DuplicateValues();
+        testSort_SingleElementArray();
+        testSort_AllEqualValues();
 
-        System.out.println("UC15 Tests Passed");
+        System.out.println("UC16 Tests Passed");
     }
 
-    static void testCargo_SafeAssignment() {
-        TrainApp.GoodsBogie g = new TrainApp.GoodsBogie("Cylindrical");
-        g.assignCargo("Petroleum");
+    static void testSort_BasicSorting() {
+        int[] arr = {72, 56, 24, 70, 60};
+        TrainApp.bubbleSort(arr);
 
-        assert "Petroleum".equals(g.cargo);
+        assert isEqual(arr, new int[]{24, 56, 60, 70, 72});
     }
 
-    static void testCargo_UnsafeAssignmentHandled() {
-        TrainApp.GoodsBogie g = new TrainApp.GoodsBogie("Rectangular");
-        g.assignCargo("Petroleum");
+    static void testSort_AlreadySortedArray() {
+        int[] arr = {24, 56, 60, 70, 72};
+        TrainApp.bubbleSort(arr);
 
-        assert g.cargo == null;
+        assert isEqual(arr, new int[]{24, 56, 60, 70, 72});
     }
 
-    static void testCargo_CargoNotAssignedAfterFailure() {
-        TrainApp.GoodsBogie g = new TrainApp.GoodsBogie("Rectangular");
-        g.assignCargo("Petroleum");
+    static void testSort_DuplicateValues() {
+        int[] arr = {72, 56, 56, 24};
+        TrainApp.bubbleSort(arr);
 
-        assert g.cargo == null;
+        assert isEqual(arr, new int[]{24, 56, 56, 72});
     }
 
-    static void testCargo_ProgramContinuesAfterException() {
-        TrainApp.GoodsBogie g1 = new TrainApp.GoodsBogie("Rectangular");
-        TrainApp.GoodsBogie g2 = new TrainApp.GoodsBogie("Cylindrical");
+    static void testSort_SingleElementArray() {
+        int[] arr = {50};
+        TrainApp.bubbleSort(arr);
 
-        g1.assignCargo("Petroleum");
-        g2.assignCargo("Coal");
-
-        assert "Coal".equals(g2.cargo);
+        assert isEqual(arr, new int[]{50});
     }
 
-    static void testCargo_FinallyBlockExecution() {
-        TrainApp.GoodsBogie g = new TrainApp.GoodsBogie("Rectangular");
-        g.assignCargo("Petroleum");
+    static void testSort_AllEqualValues() {
+        int[] arr = {40, 40, 40};
+        TrainApp.bubbleSort(arr);
 
-        assert g.finallyExecuted;
+        assert isEqual(arr, new int[]{40, 40, 40});
+    }
+
+    static boolean isEqual(int[] a, int[] b) {
+        if (a.length != b.length) return false;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != b[i]) return false;
+        }
+        return true;
     }
 }
